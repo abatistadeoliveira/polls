f2e3a41323ffc83eb679d28415f46cfac99dce11
Alexandre Oliveira <abatistadeoliveira@unomaha.edu>
2020-01-29 16:20:12 -0600

    My App first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\alexa\OneDrive\Documents\University\UnoOmaha\ISQA 8220\Projects\mysite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
